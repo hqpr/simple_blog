cf96d44bd34d6787f75540ad380f4f3cfc61c9f2
hqpr <adubnyak@gmail.com>
2018-03-15 18:38:10 +0200

    improving simple search

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,6 +1,7 @@
 import operator
 from functools import reduce
 
+import re
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
@@ -107,28 +108,58 @@ class EditBlogPostView(LoginRequiredMixin, UpdateView):
         return reverse('index')
 
 
+def normalize_query(query_string, findterms=re.compile(r'"([^"]+)"|(\S+)').findall,
+                    normspace=re.compile(r'\s{2,}').sub):
+    """
+    Splits the query string in invidual keywords, getting rid of unecessary spaces
+    and grouping quoted words together.
+    Example:
+        >>> normalize_query('  some random  words "with   quotes  " and   spaces')
+        ['some', 'random', 'words', 'with quotes', 'and', 'spaces']
+    """
+    return [normspace(' ', (t[0] or t[1]).strip()) for t in findterms(query_string)]
+
+
+def get_query(query_string, search_fields):
+
+    """
+    Returns a query, that is a combination of Q objects. That combination
+    aims to search keywords within a model by testing the given search fields.
+    """
+    query = None # Query to search for every search term
+    terms = normalize_query(query_string)
+    for term in terms:
+        or_query = None # Query to search for a given term in each field
+        for field_name in search_fields:
+            q = Q(**{"%s__icontains" % field_name: term})
+            if or_query is None:
+                or_query = q
+            else:
+                or_query = or_query | q
+        if query is None:
+            query = or_query
+        else:
+            query = query & or_query
+    return query
+
+
 class SimplerBlogSearchView(ListView):
     model = Blog
+    template_name = 'blog/blog_list.html'
     paginate_by = 3
 
     def get_queryset(self):
-        result = super(SimplerBlogSearchView, self).get_queryset()
+        queryset = super(SimplerBlogSearchView, self).get_queryset()
 
-        query = self.request.GET.get('q')
-        if query:
-            query_list = query.split()
-            result = result.filter(
-                reduce(operator.and_,
-                       (Q(title__icontains=q) for q in query_list)) |
-                reduce(operator.and_,
-                       (Q(text__icontains=q) for q in query_list)) |
-                reduce(operator.and_,
-                       (Q(author__username__icontains=q) for q in query_list)) |
-                reduce(operator.and_,
-                       (Q(category__title__icontains=q) for q in query_list))
-            )
+        found_entries = None
+        if ('q' in self.request.GET) and self.request.GET['q'].strip():
+            query_string = self.request.GET['q']
 
-        return result
+            entry_query = get_query(query_string, ['title', 'text', ])
+
+            found_entries = queryset.filter(entry_query)
+
+        return found_entries
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
